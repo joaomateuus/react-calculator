bcb9df3d2791cddf2b944e70175b11707d8cb40b
João Mateus Morais de Amaral <02758621274@imprensa.br>
2022-03-14 10:59:53 -0400

    calculator design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import './Calculator.css'
+import Container from '@mui/material/Container'
+import { Box } from "@mui/system";
 
 export default function Calculator() {
     return (
         <div>
-            <h1>meu componente</h1>
+            <Box m={5}/>
+            <Container maxWidth="xs">
+                <div className="buttons-wrapper">
+                    <Box m={12} />
+                        <h1 className="result">01</h1>
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button className="special-carac">/</button>
+                    <button className="numbers">7</button>
+                    <button className="numbers">8</button>
+                    <button className="numbers">9</button>
+                    <button className="special-carac">X</button>
+                    <button className="numbers">4</button>
+                    <button className="numbers">5</button>
+                    <button className="numbers">6</button>
+                    <button className="special-carac">-</button>
+                    <button className="numbers">3</button>
+                    <button className="numbers">1</button>
+                    <button className="numbers">2</button>
+                    <button className="special-carac">+</button>
+                    <button className="numbers">0</button>
+                    <button className="numbers">,</button>
+                    <button className="numbers" style={{visibility:'hidden'}}>,</button>
+                    <button>=</button>
+                </div>
+            </Container>
         </div>
     )
 }
